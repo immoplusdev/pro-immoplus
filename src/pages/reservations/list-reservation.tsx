2bde6cd182f1f6cef44134ae9c497f26410dbe9b
Josh <jos77marrion@gmail.com>
2024-08-28 23:21:19 +0800

    feat(reservations): remove show button
    Remove show button

--- src/pages/reservations/list-reservation.tsx
+++ src/pages/reservations/list-reservation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useTranslate, useMany, BaseRecord} from "@refinedev/core";
 import {
   useTable,
@@ -9,9 +9,11 @@ import {
   BooleanField,
   DateField, useModalForm,
 } from "@refinedev/antd";
-import {Table, Space, Tag} from "antd";
+import {Table, Space, Tag, Modal} from "antd";
 import {StatusValidationReservation, statusValidationReservation} from "@/core/domain/reservations";
 import {StatusValidationReservationTag} from "@/pages/reservations/components";
+import {defaultFormColListColProps, defaultFormColListRowProps} from "@/configs";
+import {ColList} from "@/components/layout";
 
 export const ListReservations = () => {
   const translate = useTranslate();
@@ -19,85 +21,83 @@ export const ListReservations = () => {
     syncWithLocation: true,
   });
 
-
-  return (
+    return (
       <List
         title={translate("pages.reservation.reservations")}>
         <Table {...tableProps} rowKey="id">
-          <Table.Column
-              dataIndex="datesReservation"
-              title={translate("reservations.fields.date")}
-              render={(dateObjects: Record<string, any>[])=> {
-                return (
-                    <div>
-                      {
-                        dateObjects.map((dateObject)=>(
-                            <Tag>{new Date(dateObject.date).toLocaleDateString()}</Tag>
-                        ))
-                      }
-                    </div>
-                );
-              }}
-          />
-          <Table.Column
-              dataIndex="statusReservation"
-              title={translate("reservations.fields.status_reservation")}
-          />
-
-          <Table.Column
-              dataIndex="statusFacture"
-              title={translate("reservations.fields.status_facture")}
-              render={(value: StatusValidationReservation) => <StatusValidationReservationTag
-                  statusValidation={value}/>}
-          />
-          <Table.Column
-              dataIndex={["retraitProEffectue"]}
-              title={translate("reservations.fields.retrait_pro_effectue")}
-              render={(value: any) => <BooleanField value={value} />}
-          />
-          <Table.Column
-              dataIndex="montantTotalReservation"
-              title={translate(
-                  "reservations.fields.montant_total_reservation",
-              )}
-          />
-          <Table.Column
-              dataIndex="montantReservationSansCommission"
-              title={translate(
-                  "reservations.fields.montant_reservation_sans_commission",
-              )}
-          />
-          <Table.Column
-              dataIndex="notes"
-              title={translate("fields.notes")}
-          />0
-          <Table.Column
-              dataIndex="clientPhoneNumber"
-              title={translate("fields.client_phone_number")}
-          />
-          <Table.Column
-              title={translate("table.actions")}
-              dataIndex="actions"
-              render={(_, record: BaseRecord) => (
-                  <Space>
-                    <EditButton
-                        hideText
-                        size="small"
-                        recordItemId={record.id}
-                    />
-                    <ShowButton
-                        hideText
-                        size="small"
-                        recordItemId={record.id}
-                    />
-                    <DeleteButton
-                        hideText
-                        size="small"
-                        recordItemId={record.id}
-                    />
-                  </Space>
-              )}
-          />
+              <Table.Column
+                  dataIndex="datesReservation"
+                  title={translate("reservations.fields.date")}
+                  render={(dateObjects: Record<string, any>[])=> {
+                    return (
+                        <div>
+                          {
+                            dateObjects.map((dateObject)=>(
+                                <Tag>{new Date(dateObject.date).toLocaleDateString()}</Tag>
+                            ))
+                          }
+                        </div>
+                    );
+                  }}
+              />
+              <Table.Column
+                  dataIndex="statusReservation"
+                  title={translate("reservations.fields.status_reservation")}
+              />
+              <Table.Column
+                  dataIndex="statusFacture"
+                  title={translate("reservations.fields.status_facture")}
+                  render={(value: StatusValidationReservation) => <StatusValidationReservationTag
+                      statusValidation={value}/>}
+              />
+              <Table.Column
+                  dataIndex={["retraitProEffectue"]}
+                  title={translate("reservations.fields.retrait_pro_effectue")}
+                  render={(value: any) => <BooleanField value={value} />}
+              />
+              <Table.Column
+                  dataIndex="montantTotalReservation"
+                  title={translate(
+                      "reservations.fields.montant_total_reservation",
+                  )}
+              />
+              <Table.Column
+                  dataIndex="montantReservationSansCommission"
+                  title={translate(
+                      "reservations.fields.montant_reservation_sans_commission",
+                  )}
+              />
+              <Table.Column
+                  dataIndex="notes"
+                  title={translate("fields.notes")}
+              />0
+              <Table.Column
+                  dataIndex="clientPhoneNumber"
+                  title={translate("fields.client_phone_number")}
+              />
+              <Table.Column
+                  title={translate("table.actions")}
+                  dataIndex="actions"
+                  render={(_, record: BaseRecord) => (
+                      <Space>
+                        <EditButton
+                            hideText
+                            size="small"
+                            recordItemId={record.id}
+                        />
+                        {/*<ShowButton*/}
+                        {/*    hideText*/}
+                        {/*    size="small"*/}
+                        {/*    recordItemId={record.id}*/}
+                        {/*/>*/}
+                        <DeleteButton
+                            hideText
+                            size="small"
+                            recordItemId={record.id}
+                        />
+                      </Space>
+                  )}
+              />
         </Table>
       </List>
   );
--- src/pages/reservations/show-reservation.tsx
+++ src/pages/reservations/show-reservation.tsx
@@ -1,6 +1,124 @@
 import { IResourceComponentsProps } from "@refinedev/core";
 import { AntdShowInferencer } from "@refinedev/inferencer/antd";
 
-export const ShowReservation: React.FC<IResourceComponentsProps> = () => {
-  return <AntdShowInferencer />;
+import React from "react";
+import { useShow, useTranslate, useOne } from "@refinedev/core";
+import {
+  Show,
+  TagField,
+  TextField,
+  BooleanField,
+  NumberField,
+  DateField,
+} from "@refinedev/antd";
+import { Typography } from "antd";
+import {defaultFormColListColProps, defaultFormColListRowProps} from "@/configs";
+import {ColList} from "@/components/layout";
+
+const { Title } = Typography;
+
+export const ShowReservation = () => {
+  const translate = useTranslate();
+  const { queryResult } = useShow();
+  const { data, isLoading } = queryResult;
+
+  const record = data?.data;
+
+  const { data: residenceData, isLoading: residenceIsLoading } = useOne({
+    resource: "residences",
+    id: record?.residenceId || "",
+    queryOptions: {
+      enabled: !!record,
+    },
+  });
+
+  return (
+      <Show isLoading={isLoading}>
+         <ColList rowProps={defaultFormColListRowProps} colProps={defaultFormColListColProps}>
+             <>
+                 <Title level={5}>{translate("fields.id")}</Title>
+                 <TextField value={record?.id} />
+             </>
+             <>
+             <Title level={5}>
+                 {translate("reservations.fields.status_reservation")}
+             </Title>
+             </>
+
+             <>
+             <TextField value={record?.statusReservation} />
+             <Title level={5}>
+                 {translate("reservations.fields.status_facture")}
+             </Title>
+             </>
+
+             <>
+             <TextField value={record?.statusFacture} />
+             <Title level={5}>
+                 {translate("reservations.fields.retrait_pro_effectue")}
+             </Title>
+             </>
+
+             <>
+             <BooleanField value={record?.retraitProEffectue} />
+             <Title level={5}>
+                 {translate("reservations.fields.montant_total_reservation")}
+             </Title>
+             </>
+
+             <NumberField value={record?.montantTotalReservation ?? ""} />
+             <Title level={5}>
+                 {translate(
+                     "reservations.fields.montant_reservation_sans_commission",
+                 )}
+             </Title>
+             <NumberField
+                 value={record?.montantReservationSansCommission ?? ""}
+             />
+             <Title level={5}>{translate("fields.notes")}</Title>
+             <TextField value={record?.notes} />
+             <Title level={5}>
+                 {translate("fields.client_phone_number")}
+             </Title>
+             <NumberField value={record?.clientPhoneNumber ?? ""} />
+             <Title level={5}>
+                 {translate("fields.created_at")}
+             </Title>
+             <DateField value={record?.createdAt} />
+             <Title level={5}>
+                 {translate("fields.updated_at")}
+             </Title>
+             <DateField value={record?.updatedAt} />
+             <Title level={5}>
+                 {translate("residences.fields.residence_id")}
+             </Title>
+             {residenceIsLoading ? (
+                 <>Loading...</>
+             ) : (
+                 <>
+                        <span title="Inferencer failed to render this field. (Cannot find key)">
+                            Cannot Render
+                        </span>
+                 </>
+             )}
+             <Title level={5}>{translate("fields.client")}</Title>
+             <TextField
+                 value={
+                     record?.client?.firstName + " " + record?.client?.lastName
+                 }
+             />
+             <Title level={5}>
+                 {translate("reservations.fields.proprietaire")}
+             </Title>
+             <TextField
+                 value={
+                     record?.proprietaire?.firstName +
+                     " " +
+                     record?.proprietaire?.lastName
+                 }
+             />
+         </ColList>
+      </Show>
+  );
 };
+
