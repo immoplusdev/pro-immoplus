f7293265c6d9233a667cbc61efc3d0a8f042946a
Karamoko Israel <karamokoisrael@gmail.com>
2026-01-15 01:01:22 +0800

    chore: simplify environment configuration and update API URL defaults

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -2,4 +2,4 @@ export const isDev = import.meta.env.MODE === "development";
 export const ROUTE_PREFIX = "/backoffice";
 export const PROJECT_NAME = "Immoplus";
 export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID || "xntn5Z-AEaHwy-gkPtSa"
-export const API_URL = isDev ? import.meta.env.VITE_API_URL : (import.meta.env.VITE_API_PROD_URL || "https://api-v2.immoplus.ci");
+export const API_URL = import.meta.env.VITE_API_URL || "https://api.immoplus.ci";
