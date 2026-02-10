c6dcf713fe581ee01e6d3b4791e4c1fb9a18aa6e
John <lamagniguitoure@gmail.com>
2026-02-10 22:45:28 +0000

    feat: ajouter les infos client, propriétaire et bien sur la page demandes de visites
      Afficher les cartes propriétaire, client et bien immobilier avec
      nom, email, téléphone et bouton d'accès aux détails sur la page
      d'édition des demandes de visites.

--- src/pages/demandes-visites/components/edit-read-only-fields.tsx
+++ src/pages/demandes-visites/components/edit-read-only-fields.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import {Card, Space} from "antd";
-import {DatabaseOutlined} from "@ant-design/icons";
+import {Button, Card, Space} from "antd";
+import {ArrowRightOutlined, DatabaseOutlined, HomeOutlined, TeamOutlined, UserOutlined} from "@ant-design/icons";
 import {ReadOnlyFormField} from "@/lib/ts-utilities";
+import {Link} from "react-router-dom";
 
 
 interface VisiteDataFieldProps {
@@ -9,46 +10,144 @@ interface VisiteDataFieldProps {
     demandesVisitesData?: Record<string, any>;
 }
 export const DemandeVisiteEditDataFields: React.FC<VisiteDataFieldProps> = ({ translate, demandesVisitesData }) => {
+    const bienImmobilier = demandesVisitesData?.bienImmobilier;
+    const proprietaire = demandesVisitesData?.proprietaire;
+    const client = demandesVisitesData?.client;
+
     return (
-        <Card
-            title={
-                <Space>
-                    <DatabaseOutlined />
-                    <p>{translate("demandes_visites.fields.data")}</p>
-                </Space>
-            }
-            headStyle={{ padding: "1rem", border:"0.5px solid black" }}
-            bodyStyle={{ padding: "2rem", border:"0.5px solid black",  display: "flex" , flexDirection:"row" }}
-        >
-            <Card style={{ border: "none", width: "50%", display: "flex" , flexDirection:"row"}}>
-                <ReadOnlyFormField
-                    label={translate("demandes_visites.fields.client_phone_number")}
-                    content={demandesVisitesData?.clientPhoneNumber}
-                />
-                <ReadOnlyFormField
-                    label={translate("demandes_visites.fields.notes")}
-                    content={demandesVisitesData?.notes}
-                />
-                <ReadOnlyFormField
-                    label={translate("demandes_visites.fields.retrait_pro_effectue")}
-                    content={demandesVisitesData?.retraitProEffectue ? "Oui" : "Non"}
-                />
+        <Space direction="vertical" style={{width: "100%"}} size="large">
+            <Card
+                title={
+                    <Space>
+                        <DatabaseOutlined />
+                        <p>{translate("demandes_visites.fields.data")}</p>
+                    </Space>
+                }
+                headStyle={{ padding: "1rem", border:"0.5px solid black" }}
+                bodyStyle={{ padding: "2rem", border:"0.5px solid black",  display: "flex" , flexDirection:"row" }}
+            >
+                <Card style={{ border: "none", width: "50%", display: "flex" , flexDirection:"row"}}>
+                    <ReadOnlyFormField
+                        label={translate("demandes_visites.fields.client_phone_number")}
+                        content={demandesVisitesData?.clientPhoneNumber}
+                    />
+                    <ReadOnlyFormField
+                        label={translate("demandes_visites.fields.notes")}
+                        content={demandesVisitesData?.notes}
+                    />
+                    <ReadOnlyFormField
+                        label={translate("demandes_visites.fields.retrait_pro_effectue")}
+                        content={demandesVisitesData?.retraitProEffectue ? "Oui" : "Non"}
+                    />
+                </Card>
+
+                <Card style={{ width: "50%", border: "none" }}>
+                    <ReadOnlyFormField
+                        label={translate("demandes_visites.fields.montant_total_demande_visite")}
+                        content={demandesVisitesData?.montantTotalDemandeVisite}
+                    />
+                    <ReadOnlyFormField
+                        label={translate("demandes_visites.fields.montant_demande_visite_sans_commission")}
+                        content={demandesVisitesData?.montantDemandeVisiteSansCommission}
+                    />
+                    <ReadOnlyFormField
+                        label={translate("demandes_visites.fields.created_at")}
+                        content={new Date(demandesVisitesData?.createdAt).toLocaleDateString()}
+                    />
+                </Card>
             </Card>
 
-            <Card style={{ width: "50%", border: "none" }}>
-                <ReadOnlyFormField
-                    label={translate("demandes_visites.fields.montant_total_demande_visite")}
-                    content={demandesVisitesData?.montantTotalDemandeVisite}
-                />
-                <ReadOnlyFormField
-                    label={translate("demandes_visites.fields.montant_demande_visite_sans_commission")}
-                    content={demandesVisitesData?.montantDemandeVisiteSansCommission}
-                />
-                <ReadOnlyFormField
-                    label={translate("demandes_visites.fields.created_at")}
-                    content={new Date(demandesVisitesData?.createdAt).toLocaleDateString()}
-                />
+            {/* Informations du bien */}
+            <Card
+                title={
+                    <Space>
+                        <HomeOutlined/>
+                        <p>{translate("Bien immobilier")}</p>
+                    </Space>
+                }
+                extra={
+                    bienImmobilier?.id && (
+                        <Link to={`/biens-immobiliers/edit/${bienImmobilier.id}`}>
+                            <Button type="link" icon={<ArrowRightOutlined />}>
+                                {translate("actions.details")}
+                            </Button>
+                        </Link>
+                    )
+                }
+                headStyle={{ padding: "1rem", border:"0.5px solid black"}}
+                bodyStyle={{ padding: "2rem", border:"0.5px solid black", display:"flex", flexDirection: "row" }}
+            >
+                <Card style={{border: "none", width: "50%"}}>
+                    <ReadOnlyFormField label={translate("fields.nom")} content={bienImmobilier?.nom}/>
+                    <ReadOnlyFormField label={translate("biens_immobiliers.fields.type_bien_immobilier")} content={bienImmobilier?.typeBienImmobilier}/>
+                    <ReadOnlyFormField label={translate("fields.adresse")} content={bienImmobilier?.adresse}/>
+                </Card>
+                <Card style={{border: "none", width: "50%"}}>
+                    <ReadOnlyFormField label={translate("biens_immobiliers.fields.prix")} content={bienImmobilier?.prix}/>
+                    <ReadOnlyFormField label={translate("fields.description")} content={bienImmobilier?.description}/>
+                    <ReadOnlyFormField label={translate("biens_immobiliers.fields.nombre_max_occupants")} content={bienImmobilier?.nombreMaxOccupants}/>
+                </Card>
             </Card>
-        </Card>
+
+            {/* Informations du propriétaire */}
+            <Card
+                title={
+                    <Space>
+                        <UserOutlined/>
+                        <p>{translate("Propriétaire")}</p>
+                    </Space>
+                }
+                extra={
+                    proprietaire?.id && (
+                        <Link to={`/users/edit/${proprietaire.id}`}>
+                            <Button type="link" icon={<ArrowRightOutlined />}>
+                                {translate("actions.details")}
+                            </Button>
+                        </Link>
+                    )
+                }
+                headStyle={{ padding: "1rem", border:"0.5px solid black"}}
+                bodyStyle={{ padding: "2rem", border:"0.5px solid black", display:"flex", flexDirection: "row" }}
+            >
+                <Card style={{border: "none", width: "50%"}}>
+                    <ReadOnlyFormField label={translate("fields.full_name")} content={`${proprietaire?.firstName || ''} ${proprietaire?.lastName || ''}`}/>
+                    <ReadOnlyFormField label={translate("fields.email")} content={proprietaire?.email}/>
+                </Card>
+                <Card style={{border: "none", width: "50%"}}>
+                    <ReadOnlyFormField label={translate("fields.phone_number")} content={proprietaire?.phoneNumber}/>
+                    <ReadOnlyFormField label={translate("fields.adresse")} content={proprietaire?.address}/>
+                </Card>
+            </Card>
+
+            {/* Informations du client */}
+            <Card
+                title={
+                    <Space>
+                        <TeamOutlined/>
+                        <p>{translate("fields.client")}</p>
+                    </Space>
+                }
+                extra={
+                    client?.id && (
+                        <Link to={`/users/edit/${client.id}`}>
+                            <Button type="link" icon={<ArrowRightOutlined />}>
+                                {translate("actions.details")}
+                            </Button>
+                        </Link>
+                    )
+                }
+                headStyle={{ padding: "1rem", border:"0.5px solid black"}}
+                bodyStyle={{ padding: "2rem", border:"0.5px solid black", display:"flex", flexDirection: "row" }}
+            >
+                <Card style={{border: "none", width: "50%"}}>
+                    <ReadOnlyFormField label={translate("fields.full_name")} content={`${client?.firstName || ''} ${client?.lastName || ''}`}/>
+                    <ReadOnlyFormField label={translate("fields.email")} content={client?.email}/>
+                </Card>
+                <Card style={{border: "none", width: "50%"}}>
+                    <ReadOnlyFormField label={translate("fields.phone_number")} content={client?.phoneNumber}/>
+                    <ReadOnlyFormField label={translate("fields.adresse")} content={client?.address}/>
+                </Card>
+            </Card>
+        </Space>
     );
 };
